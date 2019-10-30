33eb928c07c8b15030a5667265958c92860010c1
v-yuanye <v-yuanyeu@guazi.com>
2019-10-30 18:03:20 +0800

    登陆页面

--- src/api/api.js
+++ src/api/api.js
@@ -6,3 +6,8 @@ export function getCaptcha(phone) {
     let url = `${BASE_URL}/captcha/sent?phone=${phone}`;
     return apiAxios("GET", url)
 }
+
+export function verification(phone,code) {
+    let url = `${BASE_URL}/captcha/sent?phone=${phone}&captcha=${code}`;
+    return apiAxios("GET", url)
+}
--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import mixin, { padStr } from '@/utils/mixin';
 import { is, fromJS } from 'immutable';
 import logo from '@/images/logo.png';
+import { createHashHistory } from 'history'; // hash路由
 import "./login.scss"
 import { Button , Toast , InputItem} from 'antd-mobile';
 import { createForm } from 'rc-form';
-import { getCaptcha } from "@/api/api.js"
+import { getCaptcha , verification } from "@/api/api.js"
+
+const history = createHashHistory();
 @mixin({padStr})
 class Login extends Component {
     constructor(props) {
@@ -13,7 +16,11 @@ class Login extends Component {
         this.state = {
             isAggree : [],
             isToggleOn : true,
-            phone : ''
+            isSendOut : true,
+            phone : '15321567132',
+            loadingTime : 100,
+            timer : 60,
+            phoneCode : ['','','','']
         }
     }
     shouldComponentUpdate(nextProps, nextState) {
@@ -31,12 +38,40 @@ class Login extends Component {
             })  
         }
     };
+    handelChangeCode = (value,num) => {
+        var phoneCode = this.state.phoneCode;  
+        phoneCode[num] = value;  
+        this.setState((prevState) =>{
+            delete prevState.phoneCode;
+            return prevState;
+        })
+        this.setState({
+            phoneCode 
+        })
+        switch(num) {
+            case "0" : this.inputRefTwo.focus();break;
+            case "1" : this.inputRefThree.focus();break;
+            case "2" : this.inputRefFour.focus();break;
+            case "3" : this.verification();break;
+        }  
+    }
+    verification() { // 校检验证码
+        var phone = this.state.phone;
+        phone = phone.replace(/\s+/g,""); 
+        var captcha = this.state.phoneCode.join("");
+        verification(phone,captcha).then((data) => {
+            if(data.code === 200) {
+                localStorage.__config_center_token = this.setState.phone;
+                history.push('/');
+            }
+        })
+    }
     handelChangeVal = value => {
         this.setState({
             phone : value
         }) 
     }
-    login = e => {
+    login = e => { 
         var isAggree = this.state.isAggree;
         if(isAggree.length > 0) {
             this.setState({
@@ -46,22 +81,39 @@ class Login extends Component {
             Toast.info('请勾选同意 《用户隐私》《隐私政策》《儿童隐私政策》', 1);
         }
     }
-    goBack = e => {
-        this.setState({
-            isToggleOn : !this.state.isToggleOn
-        })
+    goBack(type){
+        if(type === '1') {
+            this.setState({
+                isToggleOn : !this.state.isToggleOn
+            })
+        }else {
+            this.setState({
+                isSendOut : !this.state.isSendOut
+            })
+        }   
     }
-    register = () => {
-        var phone = this.state.phone
+    register = () => {  
+        const phone = this.state.phone
         phone = phone.replace(/\s+/g,"");       
         if(phone.length < 11) {
             Toast.info('请输入11位手机号码', 1);
         }else {
-            getCaptcha(phone)
+            getCaptcha(phone).then((data)=> {
+                if(data.code === 200) {
+                    this.setState({
+                        loadingTime : 0,
+                        isSendOut : ! this.state.isSendOut
+                    })
+                    this.inputRefOne.focus();
+                }else {
+                    console.log(data.msg)
+                }
+            })
         }
     }
     render() {
         const isToggleOn = this.state.isToggleOn;
+        const isSendOut = this.state.isSendOut;
         const { getFieldProps } = this.props.form;
         return (
             <div className="login-body-wrap" style={{background: isToggleOn ? '#de2e21' : '#fff'}}>
@@ -88,8 +140,8 @@ class Login extends Component {
                     </div>
                 </div>
                 <div style={{display: isToggleOn ? 'none' : 'block'}}>
-                    <div className="login-phone">
-                        <h2 onClick={this.goBack}><span className="iconfont icon-flow"></span>手机号登陆</h2>
+                    <div className="login-phone" style={{display: isSendOut ? 'none' : 'block'}}>
+                        <h2 onClick={() => this.goBack('1')}><span className="iconfont icon-flow"></span>手机号登陆</h2>
                         <div className="login-tip">
                             未创建手机号登陆后将自动创建账号
                         </div>
@@ -98,6 +150,44 @@ class Login extends Component {
                         </div>
                         <div>
                             <Button className="login-button" onClick={this.register}>下一步</Button>
+                        </div>
+                    </div>
+                    <div className="verification-code login-phone" style={{display: isSendOut ? 'block' : 'none'}}>
+                        <h2 onClick={() => this.goBack('2')}><span className="iconfont icon-flow"></span>手机号验证</h2>
+                        <div style={{overflow:"hidden"}}>
+                            <p className="verification-code-tip">验证码已发送至</p>
+                            <p style={{float:"left"}}>+86 {this.state.phone.substring(0,3)}****{this.state.phone.substring(9)}</p>
+                            <p style={{float:"right"}}>{this.state.timer}s</p>
+                        </div>
+                        <div className="verification-code-input">
+                            <div className="code">
+                                <InputItem 
+                                    {...getFieldProps('number')} type="number" 
+                                    ref={el => this.inputRefOne = el}
+                                    value={this.state.phoneCode[0]} 
+                                    onChange={(e) => this.handelChangeCode(e,'0')} ></InputItem>
+                            </div>
+                            <div className="code">
+                                <InputItem 
+                                    {...getFieldProps('number')} type="number" 
+                                    ref={el => this.inputRefTwo = el}
+                                    value={this.state.phoneCode[1]} 
+                                    onChange={(e) => this.handelChangeCode(e,'1')} ></InputItem>
+                            </div>
+                            <div className="code">
+                                <InputItem 
+                                    {...getFieldProps('number')} type="number" 
+                                    ref={el => this.inputRefThree = el}
+                                    value={this.state.phoneCode[2]} 
+                                    onChange={(e) => this.handelChangeCode(e,'2')} ></InputItem>
+                            </div>
+                            <div className="code">
+                                <InputItem 
+                                    {...getFieldProps('number')} type="number" 
+                                    ref={el => this.inputRefFour = el}
+                                    value={this.state.phoneCode[3]} 
+                                    onChange={(e) => this.handelChangeCode(e,'3')} ></InputItem>
+                            </div>
                         </div>
                     </div>
                 </div>
