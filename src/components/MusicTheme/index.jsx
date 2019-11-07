a683e47e3a4374df2a9d6d0faba5b8b47c755818
v-yuanye <v-yuanyeu@guazi.com>
2019-11-07 15:50:22 +0800

    新歌部分

--- src/components/MusicTheme/index.jsx
+++ src/components/MusicTheme/index.jsx
@@ -2,16 +2,30 @@ import React from "react"
 import { Card } from 'antd-mobile';
 import "./index.scss"
 import theme from "@/images/theme.png"
-function MusicTheme({width,marginbottom}) {
+function MusicTheme({width,marginbottom,showHeader,showFooter}) {
+    const footer = () => {
+        if(showFooter) {
+            return (
+                <div className="card-tip">全球音乐排行榜，古风让你的世界变得更好，天使降落是对世界的期盼</div>
+            )
+        }else {
+            return (
+                <div className="card-tip-def">
+                    <p>标题</p>
+                    <p>信息</p>
+                </div>
+            )
+        }  
+    }
     return (
         <>
             <Card style={{width:width,marginBottom:marginbottom}}>
-                <Card.Header extra={<span>this is extra</span>}/>
+                {showHeader ? <Card.Header extra={<span>this is extra</span>}/> : null}
                 <Card.Body>
                     <img src={theme} alt=""/>
                 </Card.Body>
                 <Card.Footer 
-                    content={<div className="card-tip">全球音乐排行榜，古风让你的世界变得更好，天使降落是对世界的期盼</div>} />
+                    content={footer()} />
             </Card>
         </>
     )
--- src/components/find/index.jsx
+++ src/components/find/index.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React,{useState} from "react"
 import asyncComponent from '@/utils/asyncComponent';
 import "./index.scss"
 
@@ -7,7 +7,10 @@ const NavTag = asyncComponent(() => import("@/components/find/navTag"));
 const MusicTheme = asyncComponent(() => import("@/components/MusicTheme/index"));
 
 function Find() {
+    const [newMusicActive,setNewMusicActive] = useState(true)
+
     const arr = [1,2,3,4,5,6]
+    const arr2 = [1,2,3]
     return (
         <>
             <div style={{padding:"0.38rem 0",background:'#fff'}}>
@@ -16,19 +19,50 @@ function Find() {
             <div style={{background:"#fff",paddingBottom:'0.18rem'}}> 
                 <NavTag></NavTag>
             </div>
-            <div className="recommend">
-                <div className="recommend-tip">
-                    <p>推荐歌单</p>
-                    <p>歌单广场</p>
+            <div className="recommend-wrap">
+                <div className="recommend">
+                    <div className="recommend-tip">
+                        <p className="left-tip">推荐歌单</p>
+                        <p className="right-tip">歌单广场</p>
+                    </div>
+                    <div className="recommend-music-theme">
+                        {
+                            arr.map((value) => {
+                                return (
+                                    <MusicTheme 
+                                        showHeader={true}
+                                        showFooter={true}
+                                        width={'31.5%'}
+                                        marginbottom={"0.36rem"} 
+                                        key={value}></MusicTheme>
+                                )
+                            })
+                        }
+                    </div>
                 </div>
-                <div className="recommend-music-theme">
-                    {
-                        arr.map((value) => {
-                            return (
-                                <MusicTheme width={'31.5%'} marginbottom={"0.36rem"} key={value}></MusicTheme>
-                            )
-                        })
-                    }
+                <div className="recommend">
+                    <div className="new-music">
+                        <p className="new-music-tip">
+                            <span className={newMusicActive ? 'active' : null} onClick={() => setNewMusicActive(!newMusicActive)}>新碟</span > 
+                            <span style={{padding:'0 0.16rem'}}>|</span>   
+                            <span className={newMusicActive ? null : 'active'} onClick={() => setNewMusicActive(!newMusicActive)}>新歌</span>  
+                        </p>
+                        <p className="right-tip">更多新碟</p>
+                    </div>
+                    <div className="recommend-music-theme">
+                        {
+                            arr2.map((value) => {
+                                return (
+                                    <MusicTheme 
+                                        showHeader={false}
+                                        showFooter={false}
+                                        width={'31.5%'}
+                                        marginbottom={"0.36rem"} 
+                                        key={value}></MusicTheme>
+                                )
+                            })
+                        }
+                    </div>
                 </div>
             </div>
         </>
